0549a91cd95db293b5f472042723fa629cabdbc4
iamshahrukh <sk902224@gmail.com>
2022-03-01 21:03:09 +0500

    gu

--- src/pages/ADMIN/Annoucment/new-announcement.tsx
+++ src/pages/ADMIN/Annoucment/new-announcement.tsx
@@ -20,7 +20,7 @@ const NewAnnounment: FC<INewAnnouncment> = ({
           }}
           className="my-2 border-2 border-gray-300 px-4 rounded"
         >
-          View Alls
+          View Allss
         </button>
       </div>
       <img
